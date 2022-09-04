d040ec9f3bb5ebb6b9baf41cc978962f4433d770
Anirudh Rowjee <ani.rowjee@gmail.com>
2022-09-04 15:35:19 +0530

    [WIP] Added additional tests

--- main_test.go
+++ main_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 
@@ -18,18 +21,35 @@ CREATE TABLE IF NOT EXISTS products (
 	CONSTRAINT products_pkey PRIMARY KEY (id)
 )`
 
+// UTIL Methods for testing
 // TODO refactor these two functions, put them in `db/utils`?
 func ensureTableExists() {
 	if _, err := a.DB.Exec(tableCreationQuery); err != nil {
 		log.Fatal(err)
 	}
 }
+
 func clearTable() {
 	a.DB.Exec("DELETE FROM products")
 	a.DB.Exec("ALTER SEQUENCE products_id_seq RESTART WITH 1")
 }
 
-// testing!
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	// ResponseRecorder - executes a request using the application's router
+	// and returns the response
+	resp_recorder := httptest.NewRecorder()
+	a.Router.ServeHTTP(resp_recorder, req)
+	return resp_recorder
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	// sanity checks with error messages
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
+}
+
+// TESTS BEGIN HERE
 func TestMain(m *testing.M) {
 
 	err := godotenv.Load()
@@ -54,4 +74,44 @@ func TestMain(m *testing.M) {
 	code := m.Run()
 	clearTable()
 	os.Exit(code)
+}
+
+// Testing the Products Endpoint
+func TestEmptyTable(t *testing.T) {
+	clearTable()
+	// make sure that it's empty
+	req, _ := http.NewRequest("GET", "/products", nil)
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+	// TODO there has to be a better way to do this than to convert to string
+	if body := response.Body.String(); body != "[]" {
+		t.Errorf("Expected an Empty Array. Got %s", body)
+	}
+}
+
+// Fetching products  - Checking for a product that doesn't exist
+func TestGetNonExistentProduct(t *testing.T) {
+	clearTable()
+
+	// create and execute the request
+	req, _ := http.NewRequest("GET", "/product/11", nil)
+	response := executeRequest(req)
+
+	// check the response code
+	checkResponseCode(t, http.StatusNotFound, response.Code)
+
+	// check the body and error message
+	var m map[string]string
+	// unmarshal (de-serialize) the JSON Response into the map
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	// BUG if the error key doesn't exist, this test in itself fails
+	// TODO Refactor and fix this
+	if m["error"] != "Product not found" {
+		t.Errorf("Expected the 'error' key of the response to be set to 'Product not found'. Instead got '%s'", m["error"])
+	}
+}
+
+func TestCreateProduct(t *testing.T) {
+
 }
---
+++ start_db.sh
@@ -0,0 +1,4 @@
+docker run -it --network host -p 127.0.0.1:5432:5432 -e "POSTGRES_PASSWORD=test_password" postgres
+
+
+
